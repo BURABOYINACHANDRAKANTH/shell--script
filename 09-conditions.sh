b2f088872a11d2b39d7cfc5255c944689cd18b02
buraboyinachandrakanth <buraboyinachandrakanth@gmail.com>
2025-02-09 01:50:11 +0530

    These are shell-scripting files

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -3,8 +3,11 @@
 NUMBER=$1
 
 if [$NUMBER -gt 10]
+
 then
-  echo "given number is $NUMBER greater than 10"
+    echo "given number is $NUMBER greater than 10"
+
 else
-  echo "given number is $NUMBER less than 10"
+    echo "given number is $NUMBER less than 10"
+
 fi    
