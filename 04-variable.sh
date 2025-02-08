416fc37fef3b33e7199c3423dba017a1134f272a
buraboyinachandrakanth <buraboyinachandrakanth@gmail.com>
2025-02-09 00:53:08 +0530

    These are shell-scripting files

---
+++ 04-variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+p1=$1
+p2=$2
+
+echo "$p1:: Hello $p2, How are you?"
+echo "$p2:: Hi $p1, I am fine. How are you?"
+echo "$p1: I am fine too. how is your work?"
+echo "$p2:: not bad. I am thinking to upgrade to DevOps"
